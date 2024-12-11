bc737e3eebe270dc9b6e74d5e26c7eaf8fcea7a6
Jonathan Bruce <supimjbruce@gmail.com>
2024-12-11 13:03:36 -0500

    Updated index.js
    Good afternoon GitHub! I updated the code in this file to match the colors, font sizing, spacing and more in the branding/storyboard for trackr.
    
    Also added code to append the text to the bottom with the help of the internet 😆 
    
    Thanks for reading!
    .j.

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -13,10 +13,16 @@ import React from 'react';
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Page</Text>
+      <Text style={styles.title}>Hello</Text>
+      <Text style={styles.subTitle}> Welcome to <Text style={{ fontWeight: 'bold' }}>Trackr!</Text></Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Select a card game to get started <Text style={{fontSize: 32, fontWeight: '800'}}>v</Text></Text>
+      </View>
     </View>
   );
 }
+/* Figure out how to add inline padding to the V/arrow, or make the 'v' an Arrow. */
 
 const styles = StyleSheet.create({
   container: {
@@ -25,7 +31,34 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
+
+  title: {
     color: '#FFFADD',
+    fontSize: 64,
+    fontWeight: '800',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  subTitle: {
+    color: '#FFFADD',
+    fontSize: 32,
+    fontWeight: 'medium',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  footer: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 'auto',
+  },
+
+  footerText: {
+    color: '#FFFADD',
+    fontSize: 16,
+    marginBottom: 15,
   },
 });
