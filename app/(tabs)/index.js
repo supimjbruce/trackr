076cc0f13eb2b57a9eb3197591749ed3a336d94f
Jonathan Bruce <supimjbruce@gmail.com>
2024-12-15 00:12:25 -0500

    Updated index.js
    Updated this tonight to add in the Ionicon arrow to the text element and also fixed the footer logic to make it flex/stick to the bottom. Had to work on layering my Views.
    
    Thank you for reading!
    .j.

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -9,17 +9,20 @@ jbruce.design
 
 import {Text, View, StyleSheet} from 'react-native';
 import React from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <Text style={styles.subTitle}> Welcome to <Text style={{ fontWeight: 'bold' }}>Trackr!</Text></Text>
+      <View style={styles.introContainer}>
+        <Text style={styles.title}>Hello</Text>
+        <Text style={styles.subTitle}> Welcome to <Text style={{ fontWeight: 'bold' }}>Trackr!</Text></Text>
+      </View>
 
       <View style={styles.footer}>
-        <Text style={styles.footerText}>Select a card game to get started <Text style={{fontSize: 32, fontWeight: '800'}}>v</Text></Text>
+        <Text style={styles.footerText}>Select a card game to get started <Ionicons name={'arrow-down-sharp'} color={'22668D'} size={24}></Ionicons></Text>
       </View>
-    </View>
+    </View> /* ^ Want to figure out how to better space the arrow so it's more in line with the text. */
   );
 }
 /* Figure out how to add inline padding to the V/arrow, or make the 'v' an Arrow. */
@@ -28,6 +31,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#22668D',
+  },
+
+  introContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -50,10 +57,10 @@ const styles = StyleSheet.create({
 
   footer: {
     width: '100%',
+    height: 'auto',
     justifyContent: 'center',
     alignItems: 'center',
-    position: 'absolute',
-    bottom: 'auto',
+    marginBottom: 10,
   },
 
   footerText: {
