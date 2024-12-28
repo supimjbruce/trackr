41e9f69491db5fd86e23a4ce5363c23485f4d7e8
Jonathan Bruce <supimjbruce@gmail.com>
2024-12-27 20:41:43 -0500

    Updated lorcana.js
    Took the code from Magic.js and put it in here and updated the colors in line with the Figma design. Took out unnecessary code since once you hit 20 Lore the game is over. Need to figure out a way to stop the count from going up past that.
    
    Thank you for reading!
    .j.

--- app/(tabs)/lorcana.js
+++ app/(tabs)/lorcana.js
@@ -7,26 +7,115 @@ jbruce.design
 .j.
 - - - - - */
 
-import {Text, View, StyleSheet} from 'react-native';
-import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 
-export default function LorcanaGameplay() {
+export default function MagicGameplay() {
+  const [yourLifeTotal, setYourLifeTotal] = useState(0);
+  const [opponentsLifeTotal, setOpponentsLifeTotal] = useState(0);
+
   return (
-
-    <View style={styles.container}>
-      <Text style={styles.text}>Lorcana - Gameplay Screen</Text>
-    </View>
+      <View style={styles.container}>
+        <View>
+          <View>
+            <View style={styles.flexDirectionRow}>
+              <View style={styles.yourLifeButtons}>
+                <Button /* Should I make these a Button component with Props so the code is less? */
+                  onPress={() => setYourLifeTotal(yourLifeTotal - 1)}
+                  title="Tap to lose life!"
+                  color='#8ECDDD'
+                />
+              </View>
+              <Button
+                style={styles.buttonPadding}
+                title="     "
+              />
+              <View style={styles.yourLifeButtons}>
+                <Button
+                  onPress={() => setYourLifeTotal(yourLifeTotal + 1)}
+                  title="Tap to gain life!"
+                  color='#8ECDDD'
+                />
+              </View>
+            </View>
+          </View>
+          <Text style={styles.playerOne}>{yourLifeTotal}</Text>
+        </View>
+        <View>
+          <Text style={styles.playerTwo}>{opponentsLifeTotal}</Text>
+          <View>
+            <View style={styles.flexDirectionRow}>
+              <View style={styles.opponentsLifeButtons}>
+                <Button
+                  onPress={() => setOpponentsLifeTotal(opponentsLifeTotal + 1)}
+                  title="Tap to gain life!"
+                  color='#8ECDDD'
+                />
+              </View>
+              <Button
+                style={styles.buttonPadding}
+                title="     "
+              />
+              <View style={styles.opponentsLifeButtons}>
+                <Button
+                  onPress={() => setOpponentsLifeTotal(opponentsLifeTotal - 1)}
+                  title="Tap to lose life!"
+                  color='#8ECDDD'
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#22668D',
+    backgroundColor: '#FFCC70',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
+
+  /*text: {
     color: '#FFFADD',
+  },*/
+
+  flexDirectionRow: {
+    flexDirection: 'row',
+  },
+
+  playerOne: {
+    fontSize: 100,
+    color: "#8ECDDD",
+    alignSelf: "center",
+    transform: [
+      { rotateY: '-180deg' },
+      { scaleY: -1 },
+    ],
+  },
+
+  playerTwo: {
+    fontSize: 100,
+    color: "#8ECDDD",
+    alignSelf: "center",
+  },
+
+  buttonPadding: {
+    width: 15,
+  },
+
+  yourLifeButtons: {
+    backgroundColor: '#FFFADD',
+    padding: 10,
+    borderRadius: 5,
+    transform: [{ rotate: '180deg' }]
+  },
+
+  opponentsLifeButtons: {
+    backgroundColor: '#FFFADD',
+    padding: 10,
+    borderRadius: 5,
   },
 });
