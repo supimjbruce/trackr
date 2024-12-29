01ddbd1dcc637eb9c042bced12d91628f79d56bb
Jonathan Bruce <supimjbruce@gmail.com>
2024-12-29 12:18:47 -0500

    Updated lorcana.js
    Started off updating this code to make all of the creative stylings (borderRadius, color, backgroundColor, etc.) more like the Figma design; it's getting closer to how I want it!
    
    Thanks for reading!
    .j.

--- app/(tabs)/lorcana.js
+++ app/(tabs)/lorcana.js
@@ -19,47 +19,40 @@ export default function MagicGameplay() {
         <View>
           <View>
             <View style={styles.flexDirectionRow}>
-              <View style={styles.yourLifeButtons}>
-                <Button /* Should I make these a Button component with Props so the code is less? */
-                  onPress={() => setYourLifeTotal(yourLifeTotal - 1)}
-                  title="Tap to lose life!"
-                  color='#8ECDDD'
-                />
-              </View>
-              <Button
-                style={styles.buttonPadding}
-                title="     "
-              />
-              <View style={styles.yourLifeButtons}>
+            <View style={styles.opponentPlusButton}>
                 <Button
-                  onPress={() => setYourLifeTotal(yourLifeTotal + 1)}
-                  title="Tap to gain life!"
+                  onPress={() => setOpponentsLifeTotal(opponentsLifeTotal + 1)}
+                  title="+"
+                  color='#8ECDDD'
+                /> {/*Figure out a different way to make a button element so I can change fontSize and Weight to make it more
+                  like the Figma design, like react-native-elemements, etc.*/}
+              </View>
+              <View style={styles.opponentMinusButton}>
+                <Button /* Should I make these a Button component with Props so the code is less? */
+                  onPress={() => setOpponentsLifeTotal(opponentsLifeTotal - 1)}
+                  title="-"
                   color='#8ECDDD'
                 />
               </View>
             </View>
           </View>
-          <Text style={styles.playerOne}>{yourLifeTotal}</Text>
+          <Text style={styles.playerTwo}>{opponentsLifeTotal}</Text>
         </View>
         <View>
-          <Text style={styles.playerTwo}>{opponentsLifeTotal}</Text>
+          <Text style={styles.playerOne}>{yourLifeTotal}</Text>
           <View>
             <View style={styles.flexDirectionRow}>
-              <View style={styles.opponentsLifeButtons}>
+              <View style={styles.yourMinusButton}>
                 <Button
-                  onPress={() => setOpponentsLifeTotal(opponentsLifeTotal + 1)}
-                  title="Tap to gain life!"
+                  onPress={() => setYourLifeTotal(yourLifeTotal - 1)}
+                  title="-"
                   color='#8ECDDD'
                 />
               </View>
-              <Button
-                style={styles.buttonPadding}
-                title="     "
-              />
-              <View style={styles.opponentsLifeButtons}>
+              <View style={styles.yourPlusButton}>
                 <Button
-                  onPress={() => setOpponentsLifeTotal(opponentsLifeTotal - 1)}
-                  title="Tap to lose life!"
+                  onPress={() => setYourLifeTotal(yourLifeTotal + 1)}
+                  title="+"
                   color='#8ECDDD'
                 />
               </View>
@@ -90,16 +83,32 @@ const styles = StyleSheet.create({
     fontSize: 100,
     color: "#8ECDDD",
     alignSelf: "center",
-    transform: [
-      { rotateY: '-180deg' },
-      { scaleY: -1 },
-    ],
+    padding: 30,
+    marginTop: 30,
+    marginBottom: 30,
+    backgroundColor: '#FFFADD',
+    borderRadius: 23,
+    width: 192,
+    height: 155,
+    textAlign: 'center',
   },
 
   playerTwo: {
     fontSize: 100,
     color: "#8ECDDD",
     alignSelf: "center",
+    padding: 30,
+    marginTop: 30,
+    marginBottom: 30,
+    backgroundColor: '#FFFADD',
+    borderRadius: 23,
+    transform: [
+      { rotateY: '-180deg' },
+      { scaleY: -1 },
+    ],
+    width: 192,
+    height: 155,
+    textAlign: 'center',
   },
 
   buttonPadding: {
@@ -110,12 +119,49 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFADD',
     padding: 10,
     borderRadius: 5,
-    transform: [{ rotate: '180deg' }]
   },
 
-  opponentsLifeButtons: {
+  yourPlusButton: {
     backgroundColor: '#FFFADD',
     padding: 10,
     borderRadius: 5,
+    transform: [
+      { rotateY: '-180deg' },
+      { scaleY: -1 },
+    ],
+    marginLeft: 50,
+  },
+
+  yourMinusButton: {
+    backgroundColor: '#FFFADD',
+    padding: 10,
+    borderRadius: 5,
+    transform: [
+      { rotateY: '-180deg' },
+      { scaleY: -1 },
+    ],
+    marginRight: 50,
+  },
+
+  opponentPlusButton: {
+    backgroundColor: '#FFFADD',
+    padding: 10,
+    borderRadius: 5,
+    transform: [
+      { rotateY: '-180deg' },
+      { scaleY: -1 },
+    ],
+    marginRight: 50,
+  },
+
+  opponentMinusButton: {
+    backgroundColor: '#FFFADD',
+    padding: 10,
+    borderRadius: 5,
+    transform: [
+      { rotateY: '-180deg' },
+      { scaleY: -1 },
+    ],
+    marginLeft: 50,
   },
 });
