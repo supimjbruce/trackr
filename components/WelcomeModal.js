6b1bde5732be5aee37ef4e38e6ee6c166a685a84
Jonathan Bruce <supimjbruce@gmail.com>
2025-01-14 13:34:31 -0500

    Updated WelcomeModal.js
    Worked on this over my lunch break and made it look good! Added in better styling, a paragraph that explains what the app is (straight from the README), and made the button be styled better.
    
    Also changed the issue with the spacing realized I just had a flex: 1 in the container that I needed to remove.
    
    I like how this looks now, excited to keep working on this! Thanks!
    
    .j.

--- components/WelcomeModal.js
+++ components/WelcomeModal.js
@@ -6,39 +6,43 @@ const WelcomeModal = () => {
 const [isModalVisible, setModalVisible] = useState(true);
 
     return (
-        <View style={styles.container}> {/* Thank you Google once again for help with this code :) */}
-            <Modal
-            animationType="slide"
-            transparent={true}
-            visible={isModalVisible}
-            onRequestClose={() => setModalVisible(false)}
-            >
-                <View style={styles.modalContainer}>
-                    <View style={styles.modalContent}w>
-                        <Text style={styles.modalTitle}>Welcome to Trackr!</Text>
-                            <Pressable style={styles.closeButton} onPress={() => setModalVisible(false)}>
-                                <Text style={styles.closeButtonText}>Close</Text>
-                            </Pressable>
-                    </View>
-                </View>
-            </Modal>
-        </View>
+      <View style={styles.container}> {/* Thank you Google once again for help with this code :) */}
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent} w>
+              <Text style={styles.modalTitle}>trackr</Text>
+              <Text style={styles.modalParagraphTitle}>Welcome to trackr, a simple app for tracking<br></br>life totals for trading card games.</Text>
+              <View style={styles.buttonAlign}>
+                <Pressable style={styles.closeButton} onPress={() => setModalVisible(false)}>
+                  <Text style={styles.closeButtonText}>Close</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
 
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
     },
+
     modalContainer: {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: 'rgba(0, 0, 0, 0.6)',
     },
+    
     modalContent: {
       maxHeight: '40%',
       maxWidth: '85%',
@@ -49,24 +53,43 @@ const styles = StyleSheet.create({
       borderRadius: 20,
       justifyContent: 'center',
     },
+
     modalTitle: {
-      color: '#22668D',
-      fontSize: 28,
-      fontWeight: '600',
+      color: '#FFFADD',
+      fontSize: 64,
+      fontWeight: '800',
       justifyContent: 'center',
       alignItems: 'center',
       textAlign: 'center',
     },
-    closeButton: {
-      backgroundColor: '#8ECDDD',
-      borderRadius: 23,
-      padding: 20,
-      margin: 15,
+
+    modalParagraphTitle: {
       textAlign: 'center',
     },
-    closeButtonText: {
-        textAlign: 'center',
+
+    buttonAlign: {
+      alignItems: 'center',
     },
+
+    closeButton: {
+      height: 50,
+      width: 150,
+      maxHeight: 100,
+      maxWidth: 300,
+      backgroundColor: '#8ECDDD',
+      borderRadius: 23,
+      paddingTop: 15, /* Padding is the space inside of the element :) */
+      paddingBottom: 15,
+      paddingLeft: 5,
+      margin: 25, /* Space OUTSIDE of the element */
+      textAlign: 'center',
+    },
+
+    closeButtonText: {
+      fontSize: 16,
+      textAlign: 'center',
+    },
+
   });
 
 export default WelcomeModal;
