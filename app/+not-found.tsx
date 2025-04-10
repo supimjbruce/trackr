1aefc6ee427c38764986f648c3d5353d41f6a2f9
Jonathan Bruce <supimjbruce@gmail.com>
2025-04-10 15:34:19 -0400

    Updated +not-found.tsx
    Need to keep working on styling this, I want a bold title and then text underneath like the index.js, though I didn't have time to finish the styling right now.
    
    Thank you GitHub!
    .j.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -9,72 +9,32 @@ jbruce.design
 
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
-import {Stack, Link} from 'expo-router';
+import {Link} from 'expo-router';
 
 export default function NotFoundScreen() {
   return (
-    <>
-      <Stack.Screen options={{title: 'Error 404! Page Not Found :('}} />
-      <View style={styles.container}>
-        <Text style={styles.errorSorryText}>
-            We could not find the page you were looking for!
-            We are extremely sorry! Click the button below to go back to the home page.
-
-            Thank you and have a blessed day!
-
-            -JBruce
-            Creator of trackr
-        </Text>
-        <Link href="/" style={styles.button}>
-          Go back to the home page
-        </Link>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.text}>Page Not Found</Text>
+      <Text>Description of error and saying sorry.</Text>
+      <br></br>
+      <Text>Click the link below to go back to the home page!</Text>
+      <Link href="/" style={styles.text}>
+        Go back to the home page
+      </Link> {/*Styled like button*/}
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#22668D',
     justifyContent: 'center',
     alignItems: 'center',
+    margin: 0,
   },
 
-  button: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderRadius: 10,
-    borderColor: '#000',
-    fontSize: 20,
-    color: '#fff',
-  },
-
-  /*Inspiration from Expo Tutorial:
-
-    buttonContainer: {
-    width: 320,
-    height: 68,
-    marginHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 3,
-  },
-  button: {
-    borderRadius: 10,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-  }, */
-
-  errorSorryText: {
-    flex: 1,
-    textAlign: 'center',
-    fontSize: 16,
-    color: '#000'
+  text: {
+    color: '#FFFADD',
   },
 });
-
