43733084776e9731e3d89d32b21349d45476badf
Jonathan Bruce <supimjbruce@gmail.com>
2025-04-10 15:33:23 -0400

    Updated magic.js
    Took out unnecessary spaces and comments that weren't relevant anymore.
    
    .j.

--- app/(tabs)/magic.js
+++ app/(tabs)/magic.js
@@ -13,17 +13,13 @@ import {MyContext} from '../../components/MyContext';
 import MagicLifeModal from '../../components/MagicLifeModal';
 
 export default function MagicGameplay() {
-
   const {startingLifeTotal} = useContext(MyContext);
-
   const [yourLifeTotal, setYourLifeTotal] = useState(startingLifeTotal);
   const [opponentsLifeTotal, setOpponentsLifeTotal] = useState(startingLifeTotal)
   /*const losingLifeTotal = 0;
   const [yourLifeTextColor, setYourLifeTextColor] = useState('');
   const [opponentsLifeTextColor, setOpponentsLifeTextColor] = useState('');*/
 
-
-    // CHATGPT FOR THE WIN! I used it to help me understand the context and get this part working!
     useEffect(() => {
       setYourLifeTotal(startingLifeTotal);
       setOpponentsLifeTotal(startingLifeTotal);
@@ -159,10 +155,6 @@ const styles = StyleSheet.create({
 
   },
 
-  /*buttonPadding: {
-    width: 15,
-  },*/
-
   buttonTextStyling: {
     fontSize: 24,
     fontWeight: 900,
@@ -215,146 +207,3 @@ const styles = StyleSheet.create({
     marginLeft: 25,
   },
 });
-
-/*TODO: Update this new Magic page with the old code mixed with new logic, new modal and new design. */
-
-/* [OLD 'trackr_home.js CODE]:
-.
-.
-.
-*/
-
-/*import React, { useState } from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
-import ChooseStartingLifePopup from '/Users/jonathanbruce/Coding/FirstExpoProject/app/chooseYourStartingLife_popup.js'
-
-[TODO: Need to install and learn the React Native VSCode Extensions
-so I can start testing this code better -.j.]
-
-const firstTrackrApp = () => {
-
-    const [startingLifeTotal, setStartingLifeTotal] = useState(0);
-    const [yourLifeTotal, setYourLifeTotal] = useState(0);
-    const [opponentsLifeTotal, setOpponentsLifeTotal] = useState(0);
-
-    const handleLifeUpdate = () => {
-        setYourLifeTotal(startingLifeTotal);
-        setOpponentsLifeTotal(startingLifeTotal);
-    }; [TODO: I need to figure out how to get this code to work,
-    I need it to set the player's life totals to whatever startingLifeTotal is.]
-
-    return (
-    <View style={styles.container}>
-        <ChooseStartingLifePopup />
-        <View>
-            <View>
-                    <View>
-                        <View style={styles.flexDirectionRow}>
-                            <View style={styles.yourLifeButtons}>
-                                <Button
-                                onPress={() => setYourLifeTotal(yourLifeTotal - 1)}
-                                title="Tap to lose life!"
-                                color='white'
-                                />
-                            </View>
-                                    <Button
-                                    style={styles.buttonPadding}
-                                    title="     "
-                                    />
-                            <View style={styles.yourLifeButtons}>
-                                <Button
-                                onPress={() => setYourLifeTotal(yourLifeTotal + 1)}
-                                title="Tap to gain life!"
-                                color='white'
-                                />
-                            </View>
-                        </View>
-                    </View>
-                    <Text style={styles.playerOne}>{yourLifeTotal}</Text>
-            </View>
-            <View>
-                <Text style={styles.playerTwo}>{opponentsLifeTotal}</Text>
-                    <View>
-                        <View style={styles.flexDirectionRow}>
-                            <View style={styles.opponentsLifeButtons}>
-                                <Button
-                                onPress={() => setOpponentsLifeTotal(opponentsLifeTotal + 1)}
-                                title="Tap to gain life!"
-                                color='white'
-                                />
-                            </View>
-                                    <Button
-                                    style={styles.buttonPadding}
-                                    title="     "
-                                    />
-                            <View style={styles.opponentsLifeButtons}>
-                                <Button
-                                onPress={() => setOpponentsLifeTotal(opponentsLifeTotal - 1)}
-                                title="Tap to lose life!"
-                                color='white'
-                                />
-                            </View>
-                        </View>
-                    </View>
-            </View>
-        </View>
-    </View>
-    );
-};
-
-[Things to do: -writing this on a plane
-1. Need to make buttons to Subtract Life -> DONE!
-2. Need to test styles/style the app
-3. Keep learning and creating!]
-
-const styles = StyleSheet.create ({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "skyblue",
-        paddingBottom: 50,
-    },
-
-    flexDirectionRow: {
-      flexDirection: 'row',
-    },  
-
-    playerOne: {
-        fontSize: 100,
-        color: "black",
-        alignSelf: "center",
-        transform: [
-          {rotateY: '180deg'},
-          {scaleY: -1},
-        ]
-    },
-
-    playerTwo: {
-        fontSize: 100,
-        color: "red",
-        alignSelf: "center",
-    },
-
-    buttonPadding: {
-      width: 15,
-    },
-
-    yourLifeButtons: {
-        backgroundColor: '#595959',
-        padding: 10,
-        borderRadius: 5,
-        transform: [
-          {rotateY: '180deg'},
-          {scaleY: -1},
-        ]
-    },
-
-    opponentsLifeButtons: {
-        backgroundColor: '#595959',
-        padding: 10,
-        borderRadius: 5,
-    },
-});
-
-export default firstTrackrApp;*/
