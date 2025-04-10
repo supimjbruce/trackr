3e21c97dd688818848dfcffffd4e75d7d689135d
Jonathan Bruce <supimjbruce@gmail.com>
2025-04-10 15:33:43 -0400

    Updated _layout.tsx
    Made it so the not found page doesn't have a header
    
    .j.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout() {
     <MyProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="+not-found" options={{ headerShown: false }} />
       </Stack>
     </MyProvider>
   );
