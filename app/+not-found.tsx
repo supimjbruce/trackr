ecab8989d993050811338733c16645f7d727439d
Jonathan Bruce <supimjbruce@gmail.com>
2025-04-11 09:46:03 -0400

    Updated +not-found.tsx
    Morning GitHub; spent some time and updated the Not Found page to make the styling similar to the index.js page. I like how it looks now!
    
    .j.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -7,20 +7,22 @@ jbruce.design
 .j.
 - - - - - */
 
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
 import React from 'react';
 import {Link} from 'expo-router';
 
 export default function NotFoundScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Page Not Found</Text>
-      <Text>Description of error and saying sorry.</Text>
-      <br></br>
-      <Text>Click the link below to go back to the home page!</Text>
-      <Link href="/" style={styles.text}>
-        Go back to the home page
-      </Link> {/*Styled like button*/}
+      <Text style={styles.title}>page not found</Text>
+      <Text style={styles.subTitle}>
+        {`We can't seem to find the page you're looking for.\nClick the link below to go back to the home page!`}
+      </Text>
+      <Link href="/">
+        <Pressable style={styles.closeButton}>
+          <Text style={styles.closeButtonText}>Home</Text>
+        </Pressable>
+      </Link>
     </View>
   );
 };
@@ -34,7 +36,43 @@ const styles = StyleSheet.create({
     margin: 0,
   },
 
-  text: {
+  title: {
     color: '#FFFADD',
+    fontFamily: 'Poppins-Black',
+    fontSize: 80,
+    fontWeight: '800',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+
+  subTitle: {
+    color: '#FFFADD',
+    fontFamily: 'Poppins',
+    fontSize: 18,
+    marginTop: 20,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  closeButton: {
+    height: 50,
+    width: 150,
+    maxHeight: 100,
+    maxWidth: 300,
+    backgroundColor: '#8ECDDD',
+    borderRadius: 23,
+    paddingTop: 15, /* Padding is the space inside of the element :) */
+    paddingBottom: 15,
+    paddingLeft: 5,
+    margin: 25, /* Space OUTSIDE of the element */
+    textAlign: 'center',
+  },
+
+  closeButtonText: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
