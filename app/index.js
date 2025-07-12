6c972790dfe406205e024b20e519a3de9e9b2032
Jonathan Bruce <supimjbruce@gmail.com>
2025-07-12 17:45:53 -0400

    Updated index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,1 @@
-/*export { default } from './(tabs)/index';*/
-
-import { Text, View } from 'react-native';
-export default function Home() {
-  return <View><Text>Welcome to Trackr!</Text></View>;
-}
+export { default } from './(tabs)/index';
