d4dbf27304ff3b75f45edc5b7a846b615ab86c00
Jonathan Bruce <supimjbruce@gmail.com>
2025-07-12 11:00:57 -0400

    The great big trackr fix 7/12/25 .j.
    Good morning GitHub! On the way to the great Bruce Family Reunion, I spent some time trying to fix the GitHub Pages issue and this is where the first big push got me!
    
    Let's see if it works!
    
    .j.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,6 @@
-export { default } from './(tabs)/index';
+/*export { default } from './(tabs)/index';*/
+
+import { Text, View } from 'react-native';
+export default function Home() {
+  return <View><Text>Welcome to Trackr!</Text></View>;
+}
